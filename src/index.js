a6e34e2f6a1cf0b20456da4a205dc9b95ecb6127
Aliya <aliyuwe4ka@gmail.com>
2020-10-03 18:52:06 +0300

    feat: solve typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length) {
+        return Math.max(...array);
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0
+    }
+    let result = 0;
+    for (let counter = 0; counter < array.length; counter++) {
+        result = result + array[counter];
+    }
+    return result / array.length;
 }
